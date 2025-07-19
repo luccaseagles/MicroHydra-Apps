9af487e938ac6a8dbb8ff14f35b469b89440a623
Luccas Eagles <luccaseagles@gmail.com>
2025-07-19 14:14:16 +0100

    feat: catalog

--- app-source/Remote/Remote/remote.py
+++ app-source/Remote/Remote/remote.py
@@ -1,89 +1,8 @@
-"""A simple app to query Wikipedia for page summaries."""
-import requests, network, time, json
-from machine import Pin, freq
-from lib.display import Display
-from lib.userinput import UserInput
-from lib.hydra.config import Config
-from lib.device import Device
-from lib.hydra.popup import UIOverlay
-from font import vga1_8x16 as font
-
-ROKU_IP = "192.168.68.49"
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~ global objects/vars ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-freq(240000000)
-
-if "CARDPUTER" in Device:
-    import neopixel
-    led = neopixel.NeoPixel(Pin(21), 1, bpp=3)
-
-tft = Display(use_tiny_buf=("spi_ram" not in Device))
-
-config = Config()
-
-kb = UserInput()
-
-nic = network.WLAN(network.STA_IF)
-
-OVERLAY = UIOverlay()
-
-DISPLAY_WIDTH = Device.display_width
-DISPLAY_HEIGHT = Device.display_height
-DISPLAY_WIDTH_HALF = DISPLAY_WIDTH // 2
-DISPLAY_HEIGHT_HALF = DISPLAY_HEIGHT // 2
-
-MAX_H_CHARS = DISPLAY_WIDTH // 8
-MAX_V_LINES = DISPLAY_HEIGHT // 16
-
-def gprint(text, clr_idx=8):
-    text = str(text)
-    print(text)
-    tft.fill(config.palette[2])
-    x = DISPLAY_WIDTH_HALF - (len(text) * 4)
-    tft.text(text, x, DISPLAY_HEIGHT_HALF, config.palette[clr_idx], font=font)
-    tft.show()
-
-def errprint(text):
-    text = str(text)
-    print(text)
-    tft.fill(config.palette[1])
-    OVERLAY.error(text)
-
-def create_keypress(key):
-    url = f"http://{ROKU_IP}:8060/keypress/{key}"
-    requests.post(url)
-
-gprint('Connecting to WIFI...', clr_idx=6)
-time.sleep_ms(1000)
-
-# turn on wifi if it isn't already
-if not nic.active():
-    nic.active(True)
-
-# keep trying to connect until command works
-while True:
-    try:
-        nic.connect(config['wifi_ssid'], config['wifi_pass'])
-        break
-    except Exception as e:
-        gprint(f"Got this error while connecting: {repr(e)}", clr_idx=11)
-
-# wait until connected
-gprint(f"Waiting for connection...")
-while not nic.isconnected():
-    time.sleep_ms(100)
-
-gprint("Wifi connected", clr_idx=4)
-
-
 while True:
     keys = kb.get_new_keys()
     kb.ext_dir_keys(keys)
 
     if keys:
-        # current_text = str(keys)
-        # gprint(current_text, clr_idx=6)       
-
         # Roku key mappings
         if "UP" in keys:
             create_keypress("Up")
@@ -101,7 +20,13 @@ while True:
             create_keypress("Select")
         elif "SPC" in keys:
             create_keypress("Play")
-
+        # ~~~~ MINIMAL ADDITION: handle single character keys ~~~~~~
+        else:
+            # If keys contains a single printable character (letter/number)
+            # Modify as needed to support your kb.get_new_keys() format
+            for key in keys:
+                if len(key) == 1 and key.isprintable():
+                    # Use Roku's /keypress/Lit_<char> endpoint
+                    create_keypress(f"Lit_{key}")
 
     time.sleep_ms(10)
-
